fba7f7719a1463da75b39be5fe34f453d4d2dae4
Gabriel <gah.lucas@gmail.com>
2018-07-30 13:41:12 -0300

    Correção do método de cadastro do novo administrador.
    Signed-off-by: Gabriel <gah.lucas@gmail.com>

--- menus/menu_admin.py
+++ menus/menu_admin.py
@@ -1,19 +1,31 @@
-from classes.usuario import turn_user_in_adm
+from classes.participantes import Participante
 
 
 def menu_admin(users, user):
-    print("-- Menu Administrador do Sistema --")
-    print("1 - Cadastrar administrador de sistema")
-    print("2 - Cadastrar administrador de evento")
-    print("3 - Cadastrar evento")
-    print("4 - Remover evento")
-    print("5 - Remover usuário")
-    print("6 - Listar eventos")
-    print("7 - Exibir relatórios do sistema")
-    print("8 - Exibir relatório por evento")
-    print("9 - Deslogar")
-    option = int(input("Selecione a opção: "))
+    option = None
 
-    if option == 1:
-        user_cpf = input("Informe o CPF de quem você gostaria de tornar administador: ")
-        turn_user_in_adm(users, user_cpf)
+    while option != 9:
+        print("\n-- Menu Administrador do Sistema --")
+        print("1 - Cadastrar administrador de sistema")
+        print("2 - Cadastrar administrador de evento")
+        print("3 - Cadastrar evento")
+        print("4 - Remover evento")
+        print("5 - Remover usuário")
+        print("6 - Listar eventos")
+        print("7 - Exibir relatórios do sistema")
+        print("8 - Exibir relatório por evento")
+        print("9 - Deslogar\n")
+        option = int(input("Selecione a opção: "))
+
+        if option == 1:
+            print("Digite as informações do novo administrador")
+            new_user = Participante()
+            new_user.create_account(users)
+            users[1].append(new_user)
+
+        elif option == 2:
+            print("Digite as informações do novo administrador de eventos")
+            new_user = Participante()
+            new_user.create_account(users)
+            users[2].append(new_user)
+
